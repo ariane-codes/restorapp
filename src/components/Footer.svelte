61485e4805713ad9cbaf0505232f7abd318bb022
Ariane <ariane.ernandorena@gmail.com>
2023-04-02 13:44:59 +0100

    Styled Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,15 @@
+<script lang="ts">
+	import Brand from "./brand/Brand.svelte";
 
 
-<footer class="min-h-[10rem] bg-dark-60 mt-10">
-    FOOTER HERE
+</script>
+
+<footer class="min-h-[10rem] bg-light-120 mt-10 grid md:grid-cols-2 p-4">
+    <div class="flex items-center justify-center md:justify-start px-10">
+        <Brand height={50} />
+    </div>
+    <div class="alt-font alt-font-s flex items-center md:items-end px-10 justify-center flex-col text-dark-80">
+        <span class="py-1 md:text-right">Made with ❤️ by <a class="unset-visited text-dark-80 alt-font-s alt-font" href="https://github.com/ariane-codes">Ariane Ernandorena</a></span>
+        <span class="py-1">Developed with SvelteKit</span>
+    </div>
 </footer>
