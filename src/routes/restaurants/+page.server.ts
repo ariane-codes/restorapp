65458eaa9503c147b6d750690ed19149ea1ef710
Ariane <ariane.ernandorena@gmail.com>
2023-04-23 22:52:43 +0100

    Fixed deploy, working on upload of restaurant picture

--- src/routes/restaurants/+page.server.ts
+++ src/routes/restaurants/+page.server.ts
@@ -1,8 +1,34 @@
+import { db, storage } from '$lib/firebase/firebase.client';
 import type { Restaurant } from '$lib/models';
-import { redirect } from '@sveltejs/kit';
+import { redirect, type Actions } from '@sveltejs/kit';
+import { ref, uploadBytes } from 'firebase/storage';
 import type { PageServerLoad } from './$types';
 
+export const actions: Actions = {
+    default: async ({ request, url }) => {
 
+        const formData = await request.formData();
+        const restaurantName = formData.get("restaurantName") as string;
+        const locationDescription = formData.get("locationDescription") as string;
+        const lat = parseFloat(formData.get("lat") as string);
+        const lng = parseFloat(formData.get("lng") as string);
+        const categories = (formData.get("categories") as string).split(",");
+        const tags = (formData.get("tags") as string).split(",");
+        const price = parseInt(formData.get("price") as string);
+        const picture = formData.get("picture") as Blob;
+        console.log(picture)
+
+        const newRestaurantId = crypto.randomUUID();
+
+        // Upload image first;
+        
+        const storageRef = ref(storage, `restaurants/${newRestaurantId}.${picture.type.split("/")[1]}`);
+        
+        const snapshot = await uploadBytes(storageRef, picture);
+        console.log(snapshot.metadata);
+
+    }
+}
 
 export const load = (async ({ url, fetch }) => {
     const searchParams: URLSearchParams = url.searchParams;
--- src/routes/restaurants/new/+page.svelte
+++ src/routes/restaurants/new/+page.svelte
@@ -3,16 +3,24 @@
     import Textfield from '$lib/components/textfield/Textfield.svelte';
 	import IconButton, { Icon } from '@smui/icon-button';
     import type { PageData } from './$types';
-	import { Camera } from 'lucide-svelte';
+	import { Banknote, Camera, ChefHat, ChevronDown, ChevronUp, Tag } from 'lucide-svelte';
 	import { SvelteComponent, onMount } from 'svelte';
-    import { Loader } from '@googlemaps/js-api-loader';
-	import { initAutocompleteService } from '$lib/maps/AutocompleteService';
+    import Accordion, { Panel, Header, Content } from "@smui-extra/accordion";    import { Loader } from '@googlemaps/js-api-loader';
 	import Button from '$lib/components/button/Button.svelte';
+	import { Categories, Tags } from '$lib/models';
+	import Checkbox from '$lib/components/checkbox/Checkbox.svelte';
+	import FormField from '@smui/form-field';
+	import { Group } from '@smui/button';
+	import { enhance } from '$app/forms';
     
-    export let data: PageData;
+    // export let data: PageData;
     let fileInput: HTMLInputElement;
     let autocompleteInput: SvelteComponent;
     let files: FileList;
+
+    let categoriesOpen: boolean = false;
+    let tagsOpen: boolean = false;
+    let priceOpen: boolean = false;
 
     let restaurantName: string = "";
     let addressText: string = "";
@@ -24,7 +32,10 @@
     let autocompleteService: google.maps.places.AutocompleteService | undefined = undefined;
     let placesService: google.maps.places.PlacesService | undefined = undefined;
     let autocompleteOptions: any[];
-    
+
+    $: priceColor = [1, 2, 3, 4, 5].map((priceToCheck) =>
+        price === priceToCheck ? "secondary pressed" : "secondary"
+    );
 
     onMount(() => {
         const loader = new Loader({
@@ -69,9 +80,10 @@
 
 </script>
 
-<form class="flex flex-col items-center w-full mt-10">
+<form method="POST" action="/restaurants" enctype="multipart/form-data"
+class="flex flex-col items-center w-full mt-10" use:enhance>
 
-    <h1 class="text-secondary-100">Add a Restaurant</h1>
+    <h1 class="text-secondary-100 mb-6">Add a Restaurant</h1>
     
     <div class="my-2">
         <Textfield
@@ -97,14 +109,93 @@
                 bind:value={addressText}
                 />
         </Autocomplete>
-        <input hidden type="hidden" name="locationDescription" value={addressValue?.locationDescription}/>
-        <input hidden type="hidden" name="latLong" value={addressValue?.location}/>
-
+        <input hidden type="hidden" name="locationDescription" value={addressValue?.description}/>
+        <input hidden type="hidden" name="lat" value={addressValue?.location?.lat()}/>
+        <input hidden type="hidden" name="lat" value={addressValue?.location?.lng()}/>
     </div>
 
-    <div class="flex items-center w-1/4 min-w-[200px]">
-        <input bind:this={fileInput} hidden type="file" accept=".png,.jpg" bind:files/>
-        <IconButton on:click={() => fileInput.click()} title="Add a picture">
+    <Accordion multiple class="my-6 w-[400px]">
+        <Panel variant="unelevated" bind:open={categoriesOpen}>
+            <Header>
+                <div class="flex items-center inter text-secondary-100 tracking-normal">
+                    <ChefHat class="mr-3"/>
+                    <strong>Categories</strong>
+                </div>
+                
+                <IconButton class="text-secondary-100" toggle slot="icon" pressed={categoriesOpen}>
+                    <Icon class="material-icons" on><ChevronDown/></Icon>
+                    <Icon class="material-icons"><ChevronUp/></Icon>
+                </IconButton>
+            </Header>
+            <Content class="flex flex-col"  style={"padding-top: 0; padding-bottom: 0"}>
+                {#each Object.keys(Categories) as category}
+                    <Checkbox bind:group={categories} checked={categories.includes(category)}
+                    label={Categories[category]} color="secondary-ra" value={category}/>
+                {/each}
+            </Content>
+        </Panel>
+
+        <Panel variant="unelevated" bind:open={tagsOpen}>
+            <Header>
+                <div class="flex items-center inter text-secondary-100 tracking-normal">
+                    <Tag class="mr-3"/>
+                    <strong>Tags</strong>
+                </div>
+                
+                <IconButton class="text-secondary-100" toggle slot="icon" pressed={tagsOpen}>
+                    <Icon class="material-icons" on><ChevronDown/></Icon>
+                    <Icon class="material-icons"><ChevronUp/></Icon>
+                </IconButton>
+            </Header>
+            <Content class="flex flex-col" style={"padding-top: 0; padding-bottom: 0"}>
+                {#each Object.keys(Tags) as tag}
+                    <FormField>
+                        <Checkbox bind:group={tags} checked={tags.includes(tag)}
+                        label={Tags[tag]} color="secondary-ra" value={tag}/>
+
+                    </FormField>
+                {/each}
+                
+            </Content>
+        </Panel>
+
+        <Panel variant="unelevated" bind:open={priceOpen}>
+            <Header>
+                <div class="flex items-center inter text-secondary-100 tracking-normal">
+                    <Banknote class="mr-3"/>
+                    <strong>Price</strong>
+                </div>
+                
+                <IconButton class="text-secondary-100" toggle slot="icon" pressed={priceOpen}>
+                    <Icon class="material-icons" on><ChevronDown/></Icon>
+                    <Icon class="material-icons"><ChevronUp/></Icon>
+                </IconButton>
+            </Header>
+            <Content class="flex flex-col items-center" style={"padding-right: 0; padding-left: 0"}>
+                <Group orientation="vertical" >
+                    <Button bind:color={priceColor[0]} type="button"
+                    label="€" variant="unelevated" on:click={() => {price = 1}}/>
+                    <Button bind:color={priceColor[1]} type="button"
+                    label="€€" variant="unelevated" on:click={() => {price = 2}}/>
+                    <Button bind:color={priceColor[2]} type="button"
+                    label="€€€" variant="unelevated" on:click={() => {price = 3}}/>
+                    <Button bind:color={priceColor[3]} type="button"
+                    label="€€€€" variant="unelevated" on:click={() => {price = 4}}/>
+                    <Button bind:color={priceColor[4]} type="button"
+                    label="€€€€€" variant="unelevated" on:click={() => {price = 5}}/>
+                </Group>
+                
+            </Content>
+        </Panel>
+    </Accordion>
+
+    <input hidden type="hidden" name="categories" value={categories}/>
+    <input hidden type="hidden" name="tags" value={tags}/>
+    <input hidden type="hidden" name="price" value={price}/>
+
+    <div class="flex items-center w-[370px]">
+        <input bind:this={fileInput} hidden type="file" accept=".png,.jpg" bind:files name="picture"/>
+        <IconButton on:click={() => fileInput.click()} title="Add a picture" type="button">
             <Icon class="material-icons"><Camera/></Icon>
         </IconButton>
     
@@ -115,9 +206,7 @@
 
 
     <div class="mt-5">
-        <Button label="Create" color="secondary" width="350px"/>
+        <Button label="Create" color="secondary" width="350px" type="submit"/>
     </div>
 
-    
-    
 </form>
